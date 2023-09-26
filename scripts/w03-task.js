3db6e4a2a799f9c78d9fa8cdb4de79c4a600238c
gatlingmcgee <rzman03@aol.com>
2023-09-26 17:28:14 -0500

    updates

---
+++ scripts/w03-task.js
@@ -0,0 +1,46 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+    function add (number1, number2) {
+        const sum = number1 + number2
+        return sum 
+    }
+
+function addNumbers() {
+    const firstNumber = document.querySelector("#add1").value
+    const secondNumber = document.querySelector("#add2").value
+     const sum = add(firstNumber, secondNumber)
+     document.querySelector("#sum").value = sum
+
+ //       console.log("FIRST NUMBER:", firstNumber)
+ //       console.log("SECOND NUMBER:", secondNumber)
+ //       console.log("SUM", sum)
+}
+
+document.querySelector("#addNumbers").addEventListener('click', addnumbers)
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
